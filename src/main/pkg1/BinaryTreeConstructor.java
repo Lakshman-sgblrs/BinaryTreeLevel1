6950290a3fefd41ba78832ca6a5aa80da6307e20
unknown <kumarlakshman23@gmail.com>
2022-04-11 09:12:57 +0530

    binary tree constructor

--- src/main/pkg1/BinaryTreeConstructor.java
+++ src/main/pkg1/BinaryTreeConstructor.java
@@ -1,5 +1,85 @@
 package main.pkg1;
 
-public class BinaryTreeConstructor {
+import java.util.Stack;
 
+public class BinaryTreeConstructor {
+	// 1. Read root node
+	// 2. read left of root node and keep the state 1, if its not null
+	// 3. read right of node and keep state 2, if its not null
+	// 4. if left or roght node null increase state as 3 and pop the elements
+
+	public static class Node {
+
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data, Node left, Node right) {
+			this.data = data;
+			this.left = left;
+			this.right = right;
+
+		}
+	}
+
+	public static class Pair {
+		Node node;
+		int state;
+
+		Pair(Node node, int state) {
+			this.node = node;
+			this.state = state;
+		}
+
+	}
+	
+	public static void main(String[] args) throws Exception{
+		Integer[] arr = {50,25,12,null,null,37,30,null,null,75,62,null,70, null, null, 87,null,null};
+		
+		Node root = new Node(arr[0], null, null);
+		Pair rtp = new Pair(root, 1);
+		Stack <Pair> st = new Stack<>();
+		st.push(rtp);
+		int idx = 0;
+		while(st.size() > 0) {
+			Pair top = st.peek();
+			
+			if(top.state == 1) {
+				idx++;
+				if(arr[idx] != null) {
+					top.node.left = new Node(arr[idx], null, null);
+					Pair lp = new Pair(top.node.left, 1);
+					st.push(lp);
+				}else {
+					top.node.left = null;
+				}
+				top.state++;
+			} else if(top.state == 2) {
+				idx++;
+				if(arr[idx] != null) {
+					top.node.right = new Node(arr[idx], null, null);
+					Pair rp = new Pair(top.node.right, 1);
+					st.push(rp);
+				}else {
+					top.node.right = null;
+				}
+				top.state++;
+			}else {
+				st.pop();
+			}
+		}
+	
+	
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
